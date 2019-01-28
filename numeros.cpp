188089a91c585d45912c4464a2c67015828721a4
Rocio Crystal <rhernandez@ids.upchiapas.edu.mx>
2019-01-28 10:01:39 -0600

    cambio a rama opcion1

---
+++ numeros.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    printf("1 2 3 4 5 \n");
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
