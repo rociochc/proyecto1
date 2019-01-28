a90a6ce2d8083deaf3710ee90d8898d08dc9e179
Rocio Crystal <rhernandez@ids.upchiapas.edu.mx>
2019-01-27 21:00:38 -0600

    /agregar saludo en la siguiente actualizacio. r/en esta version se agrego la palabra "moficado" //@a
    @a

---
+++ saludo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+printf ("Hola \n");
+printf("Version modificada \n");
+
+	return 0;
+}
